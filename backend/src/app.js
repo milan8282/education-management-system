0f64475dcdccb1ac88aef6acb0a052f378a66d48
Milan Dhameliya <milandhameliya173@gmail.com>
2026-04-25 19:08:15 +0530

    added vercel.json and cors setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,9 +13,10 @@ import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
 
+// Global CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "https://education-management-system-delta.vercel.app/",
+    origin: true,
     credentials: true,
   })
 );
